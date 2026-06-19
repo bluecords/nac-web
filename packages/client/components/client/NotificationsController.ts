d276fa690de276e4570f3db8271c717bdc86d413
Jacob Schlecht <Dadadah@users.noreply.github.com>
2026-06-19 17:03:07 -0600

    fix: Add base path to service worker registration check (#1279)
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>
    (cherry picked from commit c3f91f9779888efc24e94c27c652bfaa3c2f2723)

--- packages/client/components/client/NotificationsController.ts
+++ packages/client/components/client/NotificationsController.ts
@@ -136,7 +136,9 @@ async function setUpServiceWorkerSubscription(client: Client) {
     throw "Client not configured";
   }
 
-  const registration = await navigator.serviceWorker.getRegistration();
+  const registration = await navigator.serviceWorker.getRegistration(
+    import.meta.env.BASE_URL ?? undefined,
+  );
   if (!registration) {
     throw "Failed to get service worker";
   }
@@ -179,7 +181,9 @@ export async function killServiceWorkerSubscription(client: Client) {
     return;
   }
 
-  const registration = await navigator.serviceWorker.getRegistration();
+  const registration = await navigator.serviceWorker.getRegistration(
+    import.meta.env.BASE_URL ?? undefined,
+  );
   if (!registration) return;
   const subscription = await registration.pushManager.getSubscription();
   if (await subscription?.unsubscribe()) {
